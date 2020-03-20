3ec3ef9b10809d59fdabf304ad8408b5dd36e22b
Min- <min_z@outlook.com>
2020-03-20 10:25:58 +0800

    Initial Commit

---
+++ cpphelloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpphelloworld
+//
+//  Created by Min Zhang on 2020/3/20.
+//  Copyright © 2020 Min Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
