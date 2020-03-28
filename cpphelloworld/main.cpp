168dc427b05af8444d3eb8a5db862d245486c2b7
Min- <min_z@outlook.com>
2020-03-28 12:49:43 +0800

    copying

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -10,60 +10,13 @@
 #include <string>
 #include <memory>
 
-class Entity
-{
-public:
-    Entity()
-    {
-        std::cout << "create a Entity" << std::endl;
-    }
-    
-    ~Entity()
-    {
-        std::cout << "destroy a Entity" << std::endl;
-    }
-    
-    void Print()
-    {
-        std::cout << "print function" << std::endl;
-    }
-};
-
-class ScopedPtr
-{
-private:
-    Entity* m_Ptr;
-    std::time_t m_result;
-public:
-    ScopedPtr(Entity* ptr)
-        : m_Ptr(ptr), m_result(std::time(nullptr))
-    {
-        std::cout << std::asctime(std::localtime(&m_result)) << std::endl;
-        std::cout << "scoped ptr created." << std::endl;
-    }
-    
-    ~ScopedPtr()
-    {
-        std::cout << "scoped ptr deleted." << std::endl;
-        std::cout << std::asctime(std::localtime(&m_result)) << m_result << " seconds since the Epoch\n";
-        delete m_Ptr;
-    }
-};
-
 int main()
 {
-    {
-        std::shared_ptr<Entity> shareE0;
-        {
-            std::unique_ptr<Entity> entity = std::make_unique<Entity>();
-            entity->Print();
-        }
-        {
-            std::shared_ptr<Entity> shareE = std::make_shared<Entity>();
-            shareE0 = shareE;
-            shareE0->Print();
-        }
-    }
+    const int a = 3;
+    int b = a;
+    b = 41;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
     
     return 0;
 }
