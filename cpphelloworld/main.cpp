382a3a62ed3dde9dd2e84288bfe71133be66e17d
Min- <min_z@outlook.com>
2020-03-29 22:44:58 +0800

    static array

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -8,53 +8,22 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <array>
 
-
-#ifdef MZ_DEBUG
-#define LOG(x) std::cout << x << std::endl;
-#elif defined(MZ_RELEASE)
-#define LOG(x) std::cout << "release mode." << std::endl;
-#endif
-
-
-
-template<class T>
-T addTwo(T a, T b){
-    return a + b;
-}
 
 template<typename T>
 void Print(T a){
     std::cout << a << std::endl;
 }
 
-auto AddOne(int a){
-    return a+1;
-}
-
 // pass Vector by reference to avoid copying
 int main()
 {
-    auto as = 6;
-    Print<int>(AddOne(as));
-    
-    std::vector<std::string> string;
-    string.emplace_back("Apple");
-    string.emplace_back("Orange");
-    
-    for (std::string s : string){
-        Print(s);
-    }
-    
-    for (std::vector<std::string>::iterator it = string.begin();
-         it != string.end(); it++){
-        Print(*it);
-    }
-    
-    for (auto it = string.begin(); it != string.end(); it++){
-        Print(*it);
-    }
+    std::array<int, 5> data;
+    data[12] = 5;
+    Print(data.size());
+    Print(data[12]);
+    Print(data[0]);
     
     return 0;
 }
