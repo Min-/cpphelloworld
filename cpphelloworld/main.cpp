e53c77e2935dccdf1d0c7fc19cdedbefca58c9bc
Min- <min_z@outlook.com>
2020-03-29 13:09:13 +0800

    template basic

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -10,50 +10,28 @@
 #include <string>
 #include <vector>
 
-class Vertex
+template<typename T>
+void Print(T value)
 {
-public:
-    float x, y, z;
-    
-    Vertex(float x, float y, float z)
-    : x(x), y(y), z(z){};
-    
-    // copy constructor
-    Vertex(const Vertex& vertex)
-    : x(vertex.x), y(vertex.y), z(vertex.z)
-    {
-        std::cout << "copy" << std::endl;
-    };
-};
-
-std::ostream& operator<<(std::ostream&stream, const Vertex& vertex)
-{
-    stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
-    return stream;
+    std::cout << value << std::endl;
 }
+
+template<int N>
+class Array
+{
+private:
+    int m_Array[N];
+public:
+    int GetSize() const {return N;}
+};
 
 // pass Vector by reference to avoid copying
 int main()
 {
-    Vertex v = Vertex {3,4,6.3};
-     
-    std::vector<Vertex> vertices;
-    vertices.reserve(3);
-    vertices.emplace_back(34,56,32);
-    std::cout << "line" << std::endl;
-    vertices.emplace_back(4,5,6);
-    std::cout << "line" << std::endl;
-    
-    for (int i = 0; i < vertices.size(); i++)
-        std::cout << vertices[i] << std::endl;
-    
-    for (Vertex& v : vertices)
-        std::cout << v << std::endl;
-    
-    vertices.clear();
-    
-    for (Vertex& v : vertices)
-           std::cout << v << std::endl;
-    
+    Print<int>(12);
+    Print(4.5f);
+    Print("abc");
+    Array<5>arr;
+    Print(arr.GetSize());
     return 0;
 }
