b1aaa9a58c679413e306d08bec02d2a2e149f2fb
Min- <min_z@outlook.com>
2020-03-30 23:00:26 +0800

    recursive function, fraction

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -11,70 +11,24 @@
 #include <array>
 #include <vector>
 
-typedef void(*int2void)(int); // int -> void
-typedef int(*int2int)(int); // int -> int
-
-template<typename T>
-void Print(T a){
-    std::cout << a << std::endl;
-}
-
-template<typename T1, typename T2>
-void Print2(T1 a, T2 b){
-    std::cout << a << ", " << b + 1 << std::endl;
-}
-
-void HelloWorld(int a){
-    std::cout << "Hello World! The value: " << a << std::endl;
-}
-
-int AddOne(int a){
-    return a + 1;
-}
-
-int TimesTwo(int a){
-    return a * 2;
-}
-
 // pass Vector by reference to avoid copying
-int main()
-{
-   // void(*hello)();
-    
-    int2void hello = HelloWorld;
+int frac(int x){
+    if (x == 1){
+        return 1;
+    } else {
+        return x * frac(x - 1);
+    }
+}
 
-    for (int i = 0; i < 5; i++){
-        hello(i);
+int frac2(int x){
+    int res = 1;
+    for (int i = 1; i <= x; i++){
+        res *= i;
     }
-    
-    auto addone = &AddOne;
-    Print(addone(12));
-   
-    std::vector<int2void> functions = {HelloWorld, HelloWorld};
-    for (int2void f: functions){
-        f(12);
-    }
-    
-    // a vector of function takes a function and return a function
-    std::vector<int2int> intFunctions = {AddOne, TimesTwo};
-    for (int2int f: intFunctions){
-        Print(f(12));
-    }
-    
-    Print2("hello", 3);
-    std::cout << "-------" << std::endl;
-    
-    // lambda
-    std::vector<int> values = {1,2,3,4};
-    std::vector<int> res;
-    for (int v: values){
-        res.emplace_back([](int* x)
-        {return *x+1;}(&v));
-    }
-    
-    for (int v: res) Print(v);
-    std::cout << "-------" << std::endl;
-    for (int v: values) Print(v);
-    
-    return 0;
+    return res;
+}
+
+int main(){
+   std::cout << frac2(5) << std::endl;
+   return 0;
 }
