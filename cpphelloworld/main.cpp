632e9095ca98c5baadee19f4d5706bc9642d544f
Min- <min_z@outlook.com>
2020-04-01 16:16:42 +0800

    implementation of map function with side effects;

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -10,19 +10,51 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <string>
 
-void PrintVector(std::vector<int> vector){
-    for (int v: vector){
-        std::cout << v << ", ";
-    }
-    std::cout << std::endl;
+template<class T>
+void Print(T t){
+    std::cout << t << std::endl;
 }
 
+template<typename T, typename F>
+auto map(T input, F func){
+    std::transform(input.begin(), input.end(), input.begin(), func);
+    return input;
+}
+
+template<typename T, typename F>
+auto map2(T input, F func){
+    std::transform(input.begin(), input.end(), input.begin(), func);
+    return input;
+}
+
+
 int main(){
-    int a = 50;
-    double value = (double)a;
-    //double doublev = *(double*)&a;
-    std::cout << value << std::endl;
-    //std::cout << doublev << std::endl;
+    std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<std::string> str{"Programming", "in", "a", "functional", "style."};
+    std::vector<int> strlen;
+    
+    std::transform(vec.begin(), vec.end(), vec.begin(),
+               [](int i){ return i*i;} );
+    std::transform(str.begin(), str.end(), std::back_inserter(strlen),
+      [](std::string s){ return s.length ();} );
+    
+    for (int n: vec)
+        Print(n);
+    
+    for (std::string s: str)
+        Print(s);
+    
+    for (int n: strlen)
+        Print(n);
+    
+    Print("--------------");
+    
+    auto res = map(vec, [](int x){return x * x;});
+    
+    for (auto x: res)
+        Print(x);
+    
     return 0;
 }
