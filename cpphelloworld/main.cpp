426109630e4cce2e283811634749bce43a10a81b
Min- <min_z@outlook.com>
2020-04-02 00:07:53 +0800

    io read file

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -11,50 +11,36 @@
 #include <algorithm>
 #include <functional>
 #include <string>
+#include <fstream>
+
+
+
 
 template<class T>
 void Print(T t){
     std::cout << t << std::endl;
 }
 
-template<typename T, typename F>
-auto map(T input, F func){
-    std::transform(input.begin(), input.end(), input.begin(), func);
-    return input;
-}
 
-template<typename T, typename F>
-auto map2(T input, F func){
-    std::transform(input.begin(), input.end(), input.begin(), func);
-    return input;
-}
-
-
-int main(){
-    std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::vector<std::string> str{"Programming", "in", "a", "functional", "style."};
-    std::vector<int> strlen;
+int main()
+{
+    std::string filename("/Users/min/Downloads/testtext.txt");
     
-    std::transform(vec.begin(), vec.end(), vec.begin(),
-               [](int i){ return i*i;} );
-    std::transform(str.begin(), str.end(), std::back_inserter(strlen),
-      [](std::string s){ return s.length ();} );
+    std::ifstream file(filename);
+    std::string str;
+    std::string string;
+    std::vector<std::string> file_contents;
+    while (std::getline(file, str))
+    {
+        file_contents.emplace_back(str);
+    }
     
-    for (int n: vec)
-        Print(n);
-    
-    for (std::string s: str)
-        Print(s);
-    
-    for (int n: strlen)
-        Print(n);
-    
-    Print("--------------");
-    
-    auto res = map(vec, [](int x){return x * x;});
-    
-    for (auto x: res)
-        Print(x);
+    Print(string);
+    Print(&file_contents);
+    //Print(*(&file_contents));
+    Print(&file_contents[0]);
+    Print(&file_contents[1]);
+    Print(&file_contents[2]);
     
     return 0;
 }
