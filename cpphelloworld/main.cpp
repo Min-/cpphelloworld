2bca70c6dd7d009e05847e548755741259e2644d
Min- <min_z@outlook.com>
2020-04-07 01:16:05 +0800

    dynamic casting

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -8,8 +8,36 @@
 
 #include "pch.h"
 
+class Entity{
+public:
+    virtual void PrintName(){}
+};
+
+class Player : public Entity{
+};
+
+class Enemy : public Entity{
+};
+
 int main()
 {
-    std::cout << "hello world2!" << std::endl;
+    std::unique_ptr<Player> player1;
+    Player* player2 = new Player();
+    Entity* actualEnemy = new Enemy();
+    
+    Entity* player3 = player2;
+    
+    //Player* player4 = player3; //Cannot initialize a variable of type 'Player *' with an lvalue of type 'Entity *
+    Player* player4 = dynamic_cast<Player*>(player3); // Entity needs to be polymorphic
+    Player* player5 = dynamic_cast<Player*>(actualEnemy);
+    
+    std::cout << player4 << std::endl;
+    std::cout << player5 << std::endl; // null pointer
+    
+    if (dynamic_cast<Player*>(actualEnemy)){
+        std::cout << "dynamic casting success." << std::endl;
+    } else {
+        std::cout << "return null pointer" << std::endl;
+    }
     return 0;
 }
