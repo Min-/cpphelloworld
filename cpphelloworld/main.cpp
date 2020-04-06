1d0e3163e45192d4a171c72e133030ade4b977ea
Min- <min_z@outlook.com>
2020-04-06 20:29:39 +0800

    smart pointer

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -19,67 +19,24 @@
 
 int main()
 {
-//implicit conversion
-    int a = 5;
-    double value = a;
+    int a = 3;
+    // this will create an error: being freed was not allocated, because a is deleted before a_ptr is deleted.
+    //std::unique_ptr<int> a_ptr(&a);
+    //std::cout << *a_ptr << std::endl;
     
-    //c style cast
-    double b = 2.35;
-    int valueb = (int)b;
+    std::unique_ptr<int> b_ptr(new int(a));
+    std::cout << b_ptr << std::endl;
     
-    // easy to search
-    double s = static_cast<int>(b);
+    std::unique_ptr<int> c_ptr = std::move(b_ptr);
+    std::cout << *c_ptr << std::endl;
+    //std::cout << *b_ptr << std::endl;
     
-    std::string one = "1";
-    //int oneint = static_cast<int>(one);
-    int oneint = std::stoi(one);
+    std::cout << b_ptr << std::endl; // null pointer
     
-    int twoint = 2;
-    std::string two = std::to_string(twoint);
-    
-    std::string string = "12345";
-    //const char another = *string.begin();
-    //char another = *string.begin();
-    //int another = (int)(*string.begin()); // return 49, not 1;
-    int another = (int)(*(++string.begin())) - 48; //return 2
-    //int another = (int)(*(string.begin()++)) - 48; return 1
-    
-    std::cout << another << std::endl;
-    
-    //std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char> >::iterator begin = string.begin();
-    std::string::iterator begin = string.begin();
-    while(begin != string.end()){
-        std::cout << *begin << std::endl;
-        begin++;
-    }
-    
-    
-    std::vector<int> vec = {1,2,3,4};
-    
-    std::vector<int> receiver;
-    int arr[vec.size()];
-    
-    std::vector<int>::iterator runner = vec.begin();
-    int i = 0;
-    while(runner != vec.end()){
-        receiver.emplace_back(*runner + 12);
-        std::cout << *runner + 12 << std::endl;
-        arr[i] = (*runner) * 12;
-        runner++;
-        i++;
-    }
-    
-    PrintLine();
-    
-    for (int v: receiver){
-        std::cout << v << std::endl;
-    }
-    
-    PrintLine();
-    
-    for (int v: arr){
-        std::cout << v << std::endl;
-    }
-    
+    int* d_ptr;
+    d_ptr = &a;
+    std::cout << d_ptr << std::endl;
+    std::cout << *d_ptr << std::endl;
+    std::cout << &a << std::endl;
     return 0;
 }
