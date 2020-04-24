73ca7c168eee2c1ce41e5a638c07ff82467eba6f
Min- <min_z@outlook.com>
2020-04-24 13:17:47 +0800

    STL for_each, reference operation

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -10,21 +10,20 @@
 #include <algorithm>
 //#include "pch.h"
 
+auto print = [](const int& n) { std::cout << " " << n; };
 
 int main()
 {
     int nums [] = {1,2,3,4,5};
-    const int targets [] = {5,20,18,30,1,0};
     
-    std::cout << sizeof(nums) << std::endl;
-    std::cout << sizeof(nums) / sizeof(int) << std::endl;
+    std::cout << std::all_of(nums, nums + sizeof(nums)/sizeof(int), [](int &x){return x > 1;}) << std::endl;
     
-    for (const int target : targets){
-        std::cout << "target: " << target << " " ;
-        if (std::binary_search(nums, nums + sizeof(nums)/sizeof(int), target))
-            std::cout << "found" << std::endl;
-        else
-            std::cout << "not found" << std::endl;
-    }
+    std::for_each(nums, nums + sizeof(nums)/sizeof(int), print);
+    std::cout << std::endl;
+    
+    std::for_each(nums, nums + sizeof(nums)/sizeof(int), [](int &x){x++;});
+    
+    std::for_each(nums, nums + sizeof(nums)/sizeof(int), print);
+    std::cout << std::endl;
     return 0;
 }
