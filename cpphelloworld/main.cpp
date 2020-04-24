a96e51d19bde16cfe95f3036471c5d7e1f5c9430
Min- <min_z@outlook.com>
2020-04-24 13:03:36 +0800

    STL algorithm

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -14,14 +14,17 @@
 int main()
 {
     int nums [] = {1,2,3,4,5};
-    const int target = 5;
+    const int targets [] = {5,20,18,30,1,0};
     
     std::cout << sizeof(nums) << std::endl;
     std::cout << sizeof(nums) / sizeof(int) << std::endl;
     
-    if (std::binary_search(nums, nums + sizeof(nums)/sizeof(int), target))
-        std::cout << "found" << std::endl;
-    else
-        std::cout << "not found" << std::endl;
+    for (const int target : targets){
+        std::cout << "target: " << target << " " ;
+        if (std::binary_search(nums, nums + sizeof(nums)/sizeof(int), target))
+            std::cout << "found" << std::endl;
+        else
+            std::cout << "not found" << std::endl;
+    }
     return 0;
 }
