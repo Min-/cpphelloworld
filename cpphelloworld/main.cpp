c46380e09f01f5373099b3ef523dec792efbf339
Min- <min_z@outlook.com>
2020-04-25 11:45:40 +0800

    unique_ptr 2, how to use it

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -14,7 +14,14 @@ int main()
 {
     int nums [] = {1,2,3,4,5};
     int * const p = nums;
-    std::unique_ptr<int *> up = std::make_unique<int *>(nums);
+
+    std::unique_ptr<int []> up(new int [5]);
+    up[0] = 450;
+    up[1] = 200;
+    
+    std::unique_ptr<std::vector<int>> vp{new std::vector<int>()};
+    vp->emplace_back(12);
+    vp->emplace_back(45);
     
     std::cout << *p << std::endl;
     std::cout << *(p+1) << std::endl;
@@ -23,7 +30,9 @@ int main()
     std::cout << *(p+1) << std::endl;
     std::cout << &p << std::endl;
     
-    std::cout << **up << std::endl;
+    std::cout << &up << std::endl;
+    std::cout << up[1] << std::endl;
+    std::cout << *vp->begin() << std::endl;
     
     return 0;
 }
