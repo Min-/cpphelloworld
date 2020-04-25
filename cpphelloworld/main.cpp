fdb7f4b947418a6993570a2aec1e2edac562df94
Min- <min_z@outlook.com>
2020-04-25 15:54:09 +0800

    maybe API

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -8,31 +8,122 @@
 
 #include <iostream>
 #include <algorithm>
+#include <stdexcept>
 #include "pch.h"
+
+template <typename T>
+class Maybe
+{
+private:
+    int m_default = 0;
+    
+public:
+    T Just;
+    bool Nothing;
+    
+    Maybe()
+    : Nothing(true), Just(m_default){
+        #ifdef NOT_DEBUG
+            std::cout << "create Nothing" << std::endl;
+        #endif
+    };
+    
+    Maybe(T real)
+    : Just(real), Nothing(false){};
+    
+    void Show(){
+        if (Nothing){
+            std::cout << "Nothing" << std::endl;
+        } else {
+            std::cout << "Just " << Just << std::endl;
+        }
+    }
+    
+    bool isJust(){
+        if (Nothing)
+            return false;
+        else
+            return true;
+    }
+    
+    bool isNothing(){
+        return !isJust();
+    }
+    
+    T fromJust(){
+        if (Nothing){
+            throw std::invalid_argument("receive Nothing to call fromJust.");
+        } else {
+            return Just;
+        }
+    }
+        
+};
+
+bool odd(int a){
+    return a%2 != 0;
+}
+
+//typedef bool(*int2bool)(int);
+template <typename TypeA, typename TypeB>
+TypeB maybe(TypeB defvalue, TypeB(*func)(TypeA), Maybe<TypeA> maybeValue){
+    if (maybeValue.isNothing())
+        return defvalue;
+    else {
+        return func(maybeValue.fromJust());
+    }
+}
+
+Maybe<int> MaybeDivide(int a, int b){
+    if (b != 0){
+        return Maybe<int>(a/b);
+    } else {
+        return Maybe<int>();
+    }
+}
+
+Maybe<int> SafeHead(const std::vector<int> &vec){
+    if (vec.size() == 0){
+        return Maybe<int>();
+    } else
+        return Maybe<int>(*vec.begin());
+}
 
 int main()
 {
-    int nums [] = {1,2,3,4,5};
-    int * const p = nums;
-
-    std::unique_ptr<int []> up(new int [5]);
-    up[0] = 450;
-    up[1] = 200;
+    Maybe<int> a;
+    a.Show();
     
-    std::unique_ptr<std::vector<int>> vp{new std::vector<int>()};
-    vp->emplace_back(12);
-    vp->emplace_back(45);
+    Maybe<int> b(4);
+    b.Show();
     
-    std::cout << *p << std::endl;
-    std::cout << *(p+1) << std::endl;
+    Maybe<int>res = MaybeDivide(5, 5);
+    res.Show();
     
-    *(p+1) = 20;
-    std::cout << *(p+1) << std::endl;
-    std::cout << &p << std::endl;
+    Maybe<int>res2 = MaybeDivide(5, 0);
+    res2.Show();
+    std::cout << res2.isJust() << std::endl;
+    std::cout << res2.isNothing() << std::endl;
     
-    std::cout << &up << std::endl;
-    std::cout << up[1] << std::endl;
-    std::cout << *vp->begin() << std::endl;
+    std::vector<int> emptyvec;
+    Maybe<int>out = SafeHead(emptyvec);
+    if (out.isNothing()){
+        std::cout << "this is an empty vector" << std::endl;
+        out.Show();
+    }
+    //std::cout << *emptyvec.begin() << std::endl; // not safe
+    //std::cout << "fromJust: " << out.fromJust() << std::endl;
+    
+    
+    std::vector<int> vec({3,4});
+    Maybe<int>out2 = SafeHead(vec);
+    out2.Show();
+    
+    std::cout << "fromJust: " << out2.fromJust() << std::endl;
+    
+    std::cout << "is odd: " << odd(3) << std::endl;
+    std::cout << "calculate maybe: " << maybe(false, odd, out2) << std::endl;
+    std::cout << "calculate maybe: " << maybe(false, odd, out) << std::endl;
     
     return 0;
 }
