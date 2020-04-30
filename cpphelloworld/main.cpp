ec91b438f76102039ecc24f0082e94bd03018ac7
Min- <min_z@outlook.com>
2020-04-30 14:47:37 +0800

    copy and paste a boost library (one header file) to our source code.

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -9,72 +9,16 @@
 #include <iostream>
 #include "pch.h"
 #include "Maybe.hpp"
-#include "classHeader.hpp"
+#include "for_each_n.hpp"
 
-
-Maybe<int> MaybeDivide(int a, int b){
-    if (b != 0){
-        return Maybe<int>(a/b);
-    } else {
-        return Maybe<int>();
-    }
-}
-
-Maybe<int> SafeHead(const std::vector<int> &vec){
-    if (vec.size() == 0){
-        return Maybe<int>();
-    } else
-        return Maybe<int>(*vec.begin());
-}
-
-bool odd(int a){
-    return a%2 != 0;
-}
 
 int main(){
-   Maybe<int> a;
-   a.show();
-   
-   Maybe<int> b(4);
-   b.show();
-   
-   Maybe<int>res = MaybeDivide(5, 5);
-   res.show();
-   
-   Maybe<int>res2 = MaybeDivide(5, 0);
-   res2.show();
-   std::cout << res2.isJust() << std::endl;
-   std::cout << res2.isNothing() << std::endl;
-   
-   std::vector<int> emptyvec;
-   Maybe<int>out = SafeHead(emptyvec);
-   if (out.isNothing()){
-       std::cout << "this is an empty vector" << std::endl;
-       out.show();
-   }
-   //std::cout << *emptyvec.begin() << std::endl; // not safe
-   //std::cout << "fromJust: " << out.fromJust() << std::endl;
-   
-   
-   std::vector<int> vec({3,4});
-   Maybe<int>out2 = SafeHead(vec);
-   out2.show();
-   
-   std::cout << "fromJust: " << out2.fromJust() << std::endl;
-   
-   std::cout << "is odd: " << odd(3) << std::endl;
-   std::cout << "calculate maybe: " << maybe(false, odd, out2) << std::endl;
-   std::cout << "calculate maybe: " << maybe(false, odd, out) << std::endl;
-   
-   
-   //test equality
-   bool eq = out == out2;
-   std::cout << eq << std::endl;
-   std::cout << (out == out2) << std::endl;
-   std::cout << (true == true) << std::endl;
-   std::cout << (out == a) << std::endl;
-   std::cout << (b == out2) << std::endl;
-
+    std::vector<int> vec = {1,2,3,4,5};
+    boost::algorithm::for_each_n(vec.begin(), 5, [](int x){
+        std::cout << x+1 << std::endl;
+        return x+1;});
+    
+    
     return 0;
 }
 
