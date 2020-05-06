93203e97aa079360567b821e97b0de50db19de48
Min- <min_z@outlook.com>
2020-05-06 14:05:40 +0800

    some code for chess

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -7,21 +7,56 @@
 //
 
 #include <stdio.h>
-#include <iostream>
-#include <string>
+#include <array>
 #include "pch.h"
-#include "Show.h"
-#include "Comparable.h"
 
-#define PRINTER(name) printer(#name)
-void printer(std::string obj) {
-    std::cout << obj << std::endl;
-}
+enum Color{Black, White};
+static std::array<Color, 2> colors = {Color::Black, Color::White};
 
+enum class Columns{A, B, C, D, E, F, G, H};
+static std::array<Columns, 8> columns = {Columns::A, Columns::B, Columns::C, Columns::D, Columns::E, Columns::F, Columns::G, Columns::H
+};
+
+typedef int Rows;
+static std::array<Rows, 8> rows = {1,2,3,4,5,6,7,8};
+
+enum class Pieces{Pawn, Rook, Knight, Bishop, Queen, King, NA};
+
+
+class Square{
+public:
+    bool occupied;
+    Pieces piece;
+    Columns col;
+    Rows row;
+    
+    Square(Columns col, Rows row){
+        occupied = false;
+        piece = Pieces::NA;
+        col = col;
+        row = row;
+    };
+};
+
+class Board{
+private:
+    std::vector<Square> m_board;
+public:
+    void initial_board(){
+        for (Columns col: columns){
+            for (Rows row: rows){
+                m_board.emplace_back(Square(col, row));
+            }
+        }
+    }
+    
+};
 
 int main(){
+    Board board;
+    board.initial_board();
     
+    std::cout << Color::Black << std::endl;
     return 0;
 }
-
 
