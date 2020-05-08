78cc3bfe93b673707f27b93839be0c2de449d32f
Min- <min_z@outlook.com>
2020-05-08 22:18:44 +0800

    optimized short string

--- cpphelloworld/main.cpp
+++ cpphelloworld/main.cpp
@@ -10,53 +10,16 @@
 #include <array>
 #include "pch.h"
 
-enum Color{Black, White};
-static std::array<Color, 2> colors = {Color::Black, Color::White};
-
-enum class Columns{A, B, C, D, E, F, G, H};
-static std::array<Columns, 8> columns = {Columns::A, Columns::B, Columns::C, Columns::D, Columns::E, Columns::F, Columns::G, Columns::H
-};
-
-typedef int Rows;
-static std::array<Rows, 8> rows = {1,2,3,4,5,6,7,8};
-
-enum class Pieces{Pawn, Rook, Knight, Bishop, Queen, King, NA};
-
-
-class Square{
-public:
-    bool occupied;
-    Pieces piece;
-    Columns col;
-    Rows row;
-    
-    Square(Columns col, Rows row){
-        occupied = false;
-        piece = Pieces::NA;
-        col = col;
-        row = row;
-    };
-};
-
-class Board{
-private:
-    std::vector<Square> m_board;
-public:
-    void initial_board(){
-        for (Columns col: columns){
-            for (Rows row: rows){
-                m_board.emplace_back(Square(col, row));
-            }
-        }
-    }
-    
-};
-
-int main(){
-    Board board;
-    board.initial_board();
-    
-    std::cout << Color::Black << std::endl;
-    return 0;
+void* operator new(std::size_t n) {
+    std::cout << "[Allocating " << n << " bytes]";
+    return malloc(n);
+}
+void operator delete(void* p) throw() {
+    free(p);
 }
 
+int main() {
+    for (size_t i = 0; i < 24; ++i) {
+        std::cout << i << ": " << std::string(i, '=') << std::endl;
+    }
+}
