1109c8de51c37920690a43c376b2cca984f029df
min_ <min_z@outlook.com>
2020-05-19 15:54:23 +0800

    introduce visual studio code

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,14 @@ int main() {
     }
     
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
+
+    auto num = 12;
     
     auto results = numbers | std::views::filter([](int n){ return n % 2 == 0; })
-                           | std::views::transform([](int n){ return n * 2; });
+                           | std::views::transform([=](int n){ return n * num; });
                              
-    for (auto v: results) std::cout << v << " " << std::endl;
+    for (auto v: results) std::cout << v << " ";
+    std::cout << std::endl;
     
     return 0;
 }
